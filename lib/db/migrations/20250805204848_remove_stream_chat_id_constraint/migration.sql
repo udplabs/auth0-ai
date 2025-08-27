527f3aee9349b51865bdd8a4502217c7dfa377f2
Danny Fuhriman <danny.fuhriman@okta.com>
2025-08-27 13:35:42 -0700

    db migrations

--- lib/db/migrations/20250805204848_remove_stream_chat_id_constraint/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Stream_chat_id_key";
--- lib/db/migrations/20250810003417_add_chunks/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Chunk" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "pageContent" TEXT NOT NULL,
-    "metadata" JSONB NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
