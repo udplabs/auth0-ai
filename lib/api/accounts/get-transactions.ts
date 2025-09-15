e7876f489fdb7db2c4b48935be32b9a42b54d27d
Danny Fuhriman <danny.fuhriman@okta.com>
2025-09-15 11:20:47 -0700

    add tx limiter

--- lib/api/accounts/get-transactions.ts
+++ lib/api/accounts/get-transactions.ts
@@ -3,9 +3,11 @@ import { getAccounts } from './get-accounts';
 export async function getTransactions({
 	accountId,
 	userId,
+	limit,
 }: {
 	accountId?: string;
 	userId: string;
+	limit?: number;
 }) {
 	// We are cheating for now and just fetching the account w/ transactions.
 	// Easier since we are handling permissions at the account level (for now).
@@ -17,5 +19,11 @@ export async function getTransactions({
 		return account?.transactions;
 	}
 
-	return accounts.flatMap((a) => a.transactions || []);
+	const result = accounts.flatMap((a) => a.transactions || []);
+
+	if (limit && limit > 0) {
+		return result.splice(0, limit);
+	}
+
+	return result;
 }
