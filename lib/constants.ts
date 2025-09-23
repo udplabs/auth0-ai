315e6a50b7e1c41ac98d485608b0dba2c5364845
Danny Fuhriman <danny.fuhriman@okta.com>
2025-09-22 22:50:00 -0700

    improve constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,4 @@
 export const LS_KEY_AUTH = 'auth-message-sent';
 export const LS_KEY_FIRST = 'first-message-sent';
+export const CONTENT_FIRST_MESSAGE_ID = '01K3VDGK1XKJR87HZZS1JY57HJ';
+export const CONTENT_AUTHENTICATED_MESSAGE_ID = '01K3VDJ331E72V2RE00M81J9WR';
